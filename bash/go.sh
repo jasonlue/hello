cd5a97cc8e5eb6c173fa0d4dcdb4b82a8d98b4db
Jason Lu <jason.lu@gigamon.com>
2020-04-15 16:41:10 -0700

    add git.sh

--- bash/go.sh
+++ bash/go.sh
@@ -1,4 +1,7 @@
-#install
+
+apt install curl
+
+#install go
 VERSION=1.12.17
 curl -O https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz
 tar xvf go${VERSION}.linux-amd64.tar.gz
@@ -9,4 +12,3 @@ ln -fs /lib/go-${VERSION}/bin/go /usr/bin/go
 GOPATH=/home/jasonlu/go-bindata go get -u github.com/go-bindata/go-bindata/...
 mv go-bindata /lib/go-bindata
 ln -fs /lib/go-bindata/bin/go-bindata /usr/bin/go-bindata
-
