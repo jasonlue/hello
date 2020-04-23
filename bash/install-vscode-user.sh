7562977f69b4c8cef6b57464456935f54ee46c8f
Jason Lu <jason.lu@gigamon.com>
2020-04-23 01:50:07 -0700

    enable root access to vscode on debian

--- bash/install-vscode-user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export DEBIAN_FRONTEND=noninteractive
-apt-get update \
-&& apt-get install -y --no-install-recommends apt-utils \
-&& apt-get install -y --no-install-recommends curl unzip sudo \
-&& useradd -m vscode -s /bin/bash \
-&& adduser vscode sudo \
-&& echo 'vscode ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
