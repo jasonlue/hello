4c728c4e30b4ee615fc403a9fb69198e01227dd2
Jason Lu <jason.lu@gigamon.com>
2020-07-01 01:50:10 -0700

    adjust dir

--- bash/go.sh
+++ bash/go.sh
@@ -9,6 +9,6 @@ mv go /lib/go-${VERSION}
 ln -fs /lib/go-${VERSION}/bin/go /usr/bin/go
 
 #Install go-bindata
-GOPATH=/home/jasonlu/go-bindata go get -u github.com/go-bindata/go-bindata/...
-mv go-bindata /lib/go-bindata
+GOPATH=~/go-bindata go get -u github.com/go-bindata/go-bindata/...
+mv ~/go-bindata /lib/go-bindata
 ln -fs /lib/go-bindata/bin/go-bindata /usr/bin/go-bindata
