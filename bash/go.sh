e1dbab9087baa599bb15036993afadd1bf7c991a
Jason Lu <jason.lu@gigamon.com>
2020-07-01 01:51:29 -0700

    adjust dir 2

--- bash/go.sh
+++ bash/go.sh
@@ -5,10 +5,10 @@ apt install curl
 VERSION=1.12.17
 curl -O https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz
 tar xvf go${VERSION}.linux-amd64.tar.gz
-mv go /lib/go-${VERSION}
+mv -f go /lib/go-${VERSION}
 ln -fs /lib/go-${VERSION}/bin/go /usr/bin/go
 
 #Install go-bindata
 GOPATH=~/go-bindata go get -u github.com/go-bindata/go-bindata/...
-mv ~/go-bindata /lib/go-bindata
+mv -f ~/go-bindata /lib/go-bindata
 ln -fs /lib/go-bindata/bin/go-bindata /usr/bin/go-bindata
