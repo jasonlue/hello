feb5aa9634df27934b48f8d67b5cfb80f758a9eb
Jason Lu <jason.lu@gigamon.com>
2020-10-26 00:01:28 -0700

    update

--- bash/git.sh
+++ bash/git.sh
@@ -3,7 +3,7 @@ apt install git
 
 #config git
 git config --global url.git@github.icebrg.io:.insteadOf https://github.icebrg.io/
-git config --global url.git@github.com.com:.insteadOf https://github.icebrg.com/
+git config --global url.git@github.com:.insteadOf https://github.com/
 git config --global user.name "Jason Lu"
 git config --global user.email jason.lu@gigamon.com
 git config --global push.default simple
--- bash/go.sh
+++ bash/go.sh
@@ -5,10 +5,14 @@ apt install curl
 VERSION=1.12.17
 curl -O https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz
 tar xvf go${VERSION}.linux-amd64.tar.gz
-mv -f go /lib/go-${VERSION}
+mv go /lib/go-${VERSION}
 ln -fs /lib/go-${VERSION}/bin/go /usr/bin/go
 
 #Install go-bindata
-GOPATH=~/go-bindata go get -u github.com/go-bindata/go-bindata/...
-mv -f ~/go-bindata /lib/go-bindata
+GOPATH=/home/jasonlu/go-bindata go get -u github.com/go-bindata/go-bindata/...
+mv go-bindata /lib/go-bindata
 ln -fs /lib/go-bindata/bin/go-bindata /usr/bin/go-bindata
+
+#install delve
+GOPATH=/home/jasonlu/delve go get -u github.com/go-delve/delve/cmd/dlv 
+
