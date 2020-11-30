c11a41ef99d98cd843742643457de8068aac047c
Jason Lu <jason.lu@gigamon.com>
2020-11-30 02:45:44 -0800

    add goimports

--- bash/go.sh
+++ bash/go.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 GO_VERSION="1.15.3"
-
+USER=jasonlu
 main() {
     if exists_go_version ${GO_VERSION}; then
         echo Go version ${GO_VERSION} already installed.
     else
         install_go
+        install_go_extra
     fi
+    config
 }
 install_go() {
     curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
     tar xvf go${GO_VERSION}.linux-amd64.tar.gz
     mv go /lib/go-${GO_VERSION}
-    ln -fs /lib/go-${GO_VERSION}/bin/go /usr/bin/go
-
+    ln -fs /lib/go-${GO_VERSION}/bin/go /usr/local/bin/go
+}
+install_go_extra(){
     #Install go-bindata
-    GOPATH=/home/jasonlu/go-bindata go get -u github.com/go-bindata/go-bindata/...
-    mv go-bindata /lib/go-bindata
-    ln -fs /lib/go-bindata/bin/go-bindata /usr/bin/go-bindata
-
+    GOPATH=/home/${USER}/go-bindata go get -u github.com/go-bindata/go-bindata/...
+    ln -fs /home/${USER}/go-bindata/bin/go-bindata /usr/local/bin/go-bindata
+    
     #install delve
-    GOPATH=/home/jasonlu/delve go get -u github.com/go-delve/delve/cmd/dlv
+    GOPATH=/home/${USER}/delve go get -u github.com/go-delve/delve/cmd/dlv
+    ln -fs /home/${USER}/devle/bin/dlv /usr/local/bin/dlv
+    
+    #install go-imports
+    GOPATH=/home/${USER}/goimports go get -u golang.org/x/tools/cmd/goimports
+    ln -fs /home/${USER}/goimports/bin/goimports /usr/local/bin/goimports
+}
+config(){
+    echo GOSUMDB=off >> /home/${USER}/.profile
 }
 exists_go_version() {
     version="$(go_version)"
---
+++ bash/sensors.sh
@@ -0,0 +1,1 @@
+curl -H "Authorization: IBToken iCVdJOHaU089ti9CDP9KLg" https://sensor-uat.icebrg.io/v1/sensors?include=status|jq .sensors[]
