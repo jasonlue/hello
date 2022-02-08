1adc19efe5950f4842e00f0d37e2650658e9403f
root <root@deb11build>
2022-02-08 19:58:44 +0000

    improved go.sh to install latest version from internet

--- bash/go.sh
+++ bash/go.sh
@@ -1,39 +1,96 @@
 #!/bin/bash
-GO_VERSION="1.15.10"
-USER=jlu
+SUCCESS=0
+FAIL=1
 main() {
-    if exists_go_version ${GO_VERSION}; then
-        echo Go version ${GO_VERSION} already installed.
-    else
-        install_go
-        install_go_extra
+    local version 
+    if ! get_latest_go_version version; then 
+        printf "get_latest_go_version failed."
+        return $FAIL 
+    elif exists_go_version $version; then
+        printf "Go %s  already installed.\n"  $version
+        return $SUCCESS
+    elif ! install_go $version; then
+        printf "install_go %s failed.\n" $version
+        return $FAIL 
+    elif ! setup_go_path; then 
+        printf "setup_go_path failed.\n"
+        return $FAIL
+    elif ! install_go_extra; then 
+        printf "install_go_extra failed.\n"
+        return $FAIL
+    else 
+        printf "successful!\n"
+        return $SUCCESS
     fi
-}
-install_go() {
-    cd ~
-    curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
-    tar xvf go${GO_VERSION}.linux-amd64.tar.gz
-    sudo ln -s /home/${USER}/go/bin/go /usr/bin/go
 }
 
+#########################################
+# latest_go_version version
+#########################################
+get_latest_go_version(){
+    local -n glgv_version=$1
+    glgv_version=$(curl -s https://go.dev/VERSION?m=text)
+    if (($? != $SUCCESS )); then 
+        printf "curl -s https://go.dev/VERSION?m=text failed.\n" 
+        return $FAIL
+    elif [[ -z "$glgv_version" ]]; then
+        printf "go version is empty.\n"
+        return $FAIL
+    fi 
+    glgv_version=${glgv_version#go}
+    printf "latest go version is %s\n" $glgv_version
+    return $SUCCESS
+}
+
+#########################################
+# install_go $version
+#########################################
+install_go() {
+    local version=$1
+    local tarball=go$version.linux-amd64.tar.gz
+    cd $HOME
+    if ! curl -O https://dl.google.com/go/$tarball; then 
+        printf "curl -O https://dl.google.com/go/%s failed.\n" $tarball
+        return $FAIL 
+    fi 
+    rm -rf $HOME/go
+    if ! tar -xvf $tarball; then 
+        return $FAIL 
+    fi 
+    rm $tarball
+    return $SUCCESS
+}
+
+#########################################
+# setup_go_path 
+#########################################
+setup_go_path(){
+    echo 'PATH=$HOME/go/bin:\$PATH' > /etc/profile.d/go_path.sh # setup for next login
+    . /etc/profile.d/go_path.sh # set up for now.
+}
+
+#########################################
+# install_go_extra
+#   install extra tools to /root/go/bin
+#########################################
 install_go_extra(){
     #Install go-bindata
-    GOPATH=/home/${USER}/go-bindata go get -u github.com/go-bindata/go-bindata/...
-    
+    go install github.com/go-bindata/go-bindata/...@latest
     #install delve
-    GOPATH=/home/${USER}/delve go get -u github.com/go-delve/delve/cmd/dlv
-    
+    go install github.com/go-delve/delve/cmd/dlv@latest
     #install go-imports
-    GOPATH=/home/${USER}/goimports go get -u golang.org/x/tools/cmd/goimports
+    go install golang.org/x/tools/cmd/goimports@latest
 }
+
 exists_go_version() {
-    version="$(go_version)"
-    if [[ "${version}" =~ "$1" ]]; then
-        return 0
+    local version="$(go_version)"
+    if [[ "$version" =~ "$1" ]]; then
+        return $SUCCESS
     else
-        return 1
+        return $FAIL
     fi
 }
+
 go_version() {
     if ! which go > /etc/null; then
         echo ""
